9018c66d28c276af33577bc657d98bf9d360abb0
liwaiwai <leeyanyan0_0@163.com>
2026-05-12 16:11:19 +0800

    meson.build

--- local_qemu/scripts/symlink-svm.py
+++ local_qemu/scripts/symlink-svm.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python3
 
+import json
 import os
+import shlex
+import subprocess
+import sys
 
-src = "x86_64-softmmu/qemu-system-x86_64"
-link = "bin/remote-stub"
+introspect = os.environ.get('MESONINTROSPECT')
+out = subprocess.run([*shlex.split(introspect), '--targets'],
+                     stdout=subprocess.PIPE, check=True).stdout
+targets = json.loads(out)
 
-if os.path.lexits(link):
+src = None
+for t in targets:
+    if t['name'] == 'qemu-system-x86_64' and t['filename']:
+        src = t['filename'][0]
+        break
+
+if src is None:
+    print("symlink-svm: target 'qemu-system-x86_64' not found, skipping",
+          file=sys.stderr)
+    sys.exit(0)
+
+link = 'bin/remote-stub'
+os.makedirs(os.path.dirname(link), exist_ok=True)
+if os.path.lexists(link):
     os.unlink(link)
-os.symlink(src, link)
+os.symlink(os.path.relpath(src, os.path.dirname(link)), link)
+print(f'symlink-svm: {link} -> {src}')
